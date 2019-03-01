1d00d2a5d2f59bd9b3126e31ecbeb217ea0e62be
lab302-9 <lab302-9@labeng.local>
2019-03-01 17:00:40 +0700

    false

--- pracex2_2.cpp
+++ pracex2_2.cpp
@@ -1,10 +1,13 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
 using namespace std;
-
-void sort(float x[], int N){
+template <typename T>
+void sort(T x[], int N){
 	for(int i = 1; i < N; i++){
 		for(int j = i; j > 0; j--){
-			if(x[j] < x[j-1]){
-				float temp = x[j];
+			if(x[j] > x[j-1]){
+				T temp = x[j];
 				x[j] = x[j-1];
 				x[j-1] = temp;
 			}else{
@@ -15,9 +18,38 @@ void sort(float x[], int N){
 }
 
 int main(){
-	
-	//Write your code here
-	
+	ifstream source("score1.txt");
+	ofstream x("result.txt");
+	string line;
+	float a[20];
+	int i,j=0;
+	while(getline(source,line)){
+		a[i] = atof(line.c_str());		
+		i++;
+	}
+	sort(a,20);
+	int b =1;
+	int status=0;
+	int summ =0;
+	for(int i = 0; i < 20;i++){
+		if(status == 1){
+		x << b+summ << " : "<<a[i] <<"\n";
+		status =0;
+		}
+		else{
+		x << b << " : "<<a[i] <<"\n";
+		}
+		if(a[i+1]==a[i]){
+		summ = summ+1;
+		status == 1;
+		continue;
+		}
+		b++;
+		
+	}
 	return 0;
 }
+
+
+
 
